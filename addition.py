81278e61ba5034a0ef44a9e1ac106e894ec9b0cd
test <mohamed@example.com>
2025-04-04 12:21:50 +0200

    v5 : Ajout d’une interface graphique

--- addition.py
+++ addition.py
@@ -1,6 +1,25 @@
+import tkinter as tk
+
 def addition(a, b):
     return a + b
-    
-if __name__ == "__main__":
-    print(addition(2, 3))
-    print(addition(2, 5))
+
+def calculer():
+    a = int(entry1.get())
+    b = int(entry2.get())
+    resultat.set(str(addition(a, b)))
+
+app = tk.Tk()
+app.title("Additionneur")
+
+entry1 = tk.Entry(app)
+entry1.pack()
+
+entry2 = tk.Entry(app)
+entry2.pack()
+
+resultat = tk.StringVar()
+tk.Label(app, textvariable=resultat).pack()
+
+tk.Button(app, text="Calculer", command=calculer).pack()
+
+app.mainloop()
